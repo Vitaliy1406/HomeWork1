7d4d5769b872dd62171a84d2d1d5714aeb6766e2
Vitaliy1406 <126714610+Vitaliy1406@users.noreply.github.com>
2023-03-07 22:40:03 +0200

    Create Program.cs

---
+++ lesson2/Program.cs
@@ -0,0 +1,24 @@
+﻿//int x = 9;
+//double result;
+//result = (-6 * Math.Pow(x, 3) + 5 * Math.Pow(x, 2) - 10 * x + 15);
+//int a = 1;
+//double b = 1.0;
+// Console.Writeline(result);
+//int x = 15;
+//double result;
+//result = Math.Abs(x) * Math.Sin(x);
+//Console.WriteLine(result);
+//3
+//int x = 10;
+//double result;
+//result = (2 * Math.PI * x);
+//Console.WriteLine(result);
+int x = 8;
+int y = 14;
+double result;
+result = (Math.Max(x, y));
+Console.WriteLine(result);
+
+
+
+
