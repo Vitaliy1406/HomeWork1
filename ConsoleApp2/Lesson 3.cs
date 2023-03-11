3a6d7bb5e150af987fcef56f5c35f69558773197
Vitaliy1406 <126714610+Vitaliy1406@users.noreply.github.com>
2023-03-11 20:28:50 +0200

    Update Lesson 3.cs

--- ConsoleApp2/Lesson 3.cs
+++ ConsoleApp2/Lesson 3.cs
@@ -1,23 +1,55 @@
 ﻿
 
-int x = 10;
-int y = 12;
-int sum = sumBetween(10, 12);
+//int x = 10;
+//int y = 12;
+//int sum = sumBetween(10, 12);
 
+//static int sumBetween(int x, int y)
+//{
+//    int sum = 0;
+
+//    if (x <= y) ;
+
+//        for (int i = x; i <= y; i++) 
+//        {
+//            sum += i;
+//        }
+
+
+//    return sum;
+
+//}
+
+//Console.WriteLine(sum);
+//TASK 2
+
+int x = 5;
+int y = 2;
+int sum = sumBetween(5, 2);
 static int sumBetween(int x, int y)
 {
     int sum = 0;
-    
     if (x <= y) ;
-    
-        for (int i = x; i <= y; i++) 
-        {
-            sum += i;
-        }
-    
 
+        for(int i = y;i <=x;i++)
+    {
+        sum += i;
+    }
     return sum;
-
 }
-
 Console.WriteLine(sum);
+//TASK 3
+
+//int x = 10;
+//int y = 10;
+//int sum = sumBetween(10, 10);
+//static int sumBetween(int x, int y)
+//{
+//    int sum = 0;
+//    if (x == y) ;
+//    {
+//        sum = x;
+//            }
+//    return sum;
+//}
+//Console.WriteLine(sum);
