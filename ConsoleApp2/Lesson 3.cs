9929bf3d8eca38869d8b181860b5c5c354ee3db1
Vitaliy1406 <126714610+Vitaliy1406@users.noreply.github.com>
2023-03-11 14:15:40 +0200

    Create Lesson 3.cs

---
+++ ConsoleApp2/Lesson 3.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int x = 10;
+int y = 12;
+int sum = sumBetween(10, 12);
+
+static int sumBetween(int x, int y);
+{
+    int sum = 0;
+    
+    if (x <= y) ;
+    {
+        for (int i = x; i <= y; i++) ;
+        {
+            sum += i;
+        }
+    }
+
+    return sum;
+
+}
+
+Console.WriteLine(sum);
+//static int SumBetween(int x, int y);
+//{
+//    int sum = 0;
+//}
