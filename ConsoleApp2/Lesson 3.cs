a43e7b9546cbfef62bcbe86cdce9ca224e92b98f
Vitaliy1406 <126714610+Vitaliy1406@users.noreply.github.com>
2023-03-11 19:37:55 +0200

    Update Lesson 3.cs

--- ConsoleApp2/Lesson 3.cs
+++ ConsoleApp2/Lesson 3.cs
@@ -4,24 +4,20 @@ int x = 10;
 int y = 12;
 int sum = sumBetween(10, 12);
 
-static int sumBetween(int x, int y);
+static int sumBetween(int x, int y)
 {
     int sum = 0;
     
     if (x <= y) ;
-    {
-        for (int i = x; i <= y; i++) ;
+    
+        for (int i = x; i <= y; i++) 
         {
             sum += i;
         }
-    }
+    
 
     return sum;
 
 }
 
 Console.WriteLine(sum);
-//static int SumBetween(int x, int y);
-//{
-//    int sum = 0;
-//}
